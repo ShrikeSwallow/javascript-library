3e3b43ad5855bb541af3c06c3c1015269038f658
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-03 10:11:49 +0000

    Implement book adding form and button

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const myLibrary = [];
 const list = document.querySelector(".list");
-const newBook = document.querySelector(".new-book");
+const newBook = document.querySelector(".new-book-btn");
 
-const Book = (title, author, pages, readStatus) => {
+const formPanel = document.querySelector("aside");
+const form = document.querySelector("form");
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+const answer = document.querySelector("#answer");
+
+function Book(title, author, pages, readStatus) {
   //constructor
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus.toLowerCase();
   this.info = () => {
-    if (this.readStatus === "yes")
-      return `${this.title} by ${this.author}, ${this.pages} pages, already read.`;
-    else
-      return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.readStatus}`;
   };
-};
+}
 
 const addBookToLibrary = (book) => {
   myLibrary.push(book);
@@ -23,9 +27,31 @@ const addBookToLibrary = (book) => {
 const displayAllBooks = () => {
   list.innerHTML = "";
   myLibrary.forEach((book) => {
-    console.log(book.info());
+    //console.log(book.info());
     const listItem = document.createElement("li");
     listItem.textContent = book.info();
     list.appendChild(listItem);
   });
 };
+
+newBook.addEventListener("click", () => {
+  form.reset();
+  formPanel.classList.remove("hidden");
+  bookTitle.focus();
+});
+form.addEventListener("reset", () => {
+  formPanel.classList.add("hidden");
+});
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let newBook = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    answer.value
+  );
+  addBookToLibrary(newBook);
+  console.log(myLibrary);
+  formPanel.classList.add("hidden");
+  displayAllBooks();
+});
