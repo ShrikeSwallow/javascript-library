db667d49b6a80216c4ac94dc2862d52339fe3b5c
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-06 20:23:50 +0000

    Refactor code into classes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const myLibrary = [];
 const list = document.querySelector(".list");
 const newBook = document.querySelector(".new-book-btn");
 
@@ -12,16 +11,60 @@ const answer = document.querySelector("#answer");
 let statusBtn = document.querySelectorAll(".status-btn");
 let deleteBtn = document.querySelectorAll(".delete-btn");
 
-function Book(title, author, pages, readStatus) {
-  //constructor
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readStatus = readStatus.toLowerCase();
-  this.info = () => {
+class Library {
+  constructor() {
+    this.library = [];
+  }
+  addBookToLibrary = (book) => {
+    this.library.push(book);
+  };
+  displayAllBooks = () => {
+    list.innerHTML = "";
+    this.library.forEach((book) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = book.info();
+
+      const status = document.createElement("button");
+      status.classList.add("status-btn");
+      status.textContent = "Change read status";
+      listItem.appendChild(status);
+      const delBtn = document.createElement("button");
+      delBtn.classList.add("delete-btn");
+      delBtn.textContent = "Remove from Library";
+      listItem.appendChild(delBtn);
+
+      list.appendChild(listItem);
+    });
+
+    statusBtn = document.querySelectorAll(".status-btn");
+    statusBtn.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        myLibrary.library[index].toggleRead();
+        myLibrary.displayAllBooks();
+      });
+    });
+
+    deleteBtn = document.querySelectorAll(".delete-btn");
+    deleteBtn.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        myLibrary.library.splice(index, 1);
+        myLibrary.displayAllBooks();
+      });
+    });
+  };
+}
+
+class Book {
+  constructor(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus.toLowerCase();
+  }
+  info = () => {
     return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.readStatus}`;
   };
-  this.toggleRead = () => {
+  toggleRead = () => {
     if (this.readStatus === "yes") {
       this.readStatus = "no";
     } else {
@@ -30,47 +73,7 @@ function Book(title, author, pages, readStatus) {
   };
 }
 
-const addBookToLibrary = (book) => {
-  myLibrary.push(book);
-};
-
-const displayAllBooks = () => {
-  list.innerHTML = "";
-  myLibrary.forEach((book, index) => {
-    //console.log(book.info());
-    const listItem = document.createElement("li");
-    listItem.textContent = book.info();
-
-    const status = document.createElement("button");
-    status.classList.add("status-btn");
-    status.textContent = "Change read status";
-    listItem.appendChild(status);
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("delete-btn");
-    delBtn.textContent = "Remove from Library";
-    listItem.appendChild(delBtn);
-
-    list.appendChild(listItem);
-  });
-
-  statusBtn = document.querySelectorAll(".status-btn");
-  statusBtn.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      myLibrary[index].toggleRead();
-      displayAllBooks();
-    });
-  });
-
-  deleteBtn = document.querySelectorAll(".delete-btn");
-  deleteBtn.forEach((button, index) => {
-    button.addEventListener("click", (event) => {
-      console.log(`clicked, index: ${index}`);
-      myLibrary.splice(index, 1);
-      displayAllBooks();
-    });
-  });
-};
-
+const myLibrary = new Library();
 newBook.addEventListener("click", () => {
   form.reset();
   formPanel.classList.remove("hidden");
@@ -81,13 +84,13 @@ form.addEventListener("reset", () => {
 });
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let newBook = new Book(
+  const newBook = new Book(
     bookTitle.value,
     bookAuthor.value,
     bookPages.value,
     answer.value
   );
-  addBookToLibrary(newBook);
+  myLibrary.addBookToLibrary(newBook);
   formPanel.classList.add("hidden");
-  displayAllBooks();
+  myLibrary.displayAllBooks();
 });
